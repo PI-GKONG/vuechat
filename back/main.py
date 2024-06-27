d50fb96d5fc3b5ebd978f9601052e82e13c06a1d
PigKong <112669656+PI-GKONG@users.noreply.github.com>
2024-06-27 16:41:19 +0900

    V0.1.2

--- back/main.py
+++ back/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List, Dict
 import json
+import time
 
 app = FastAPI()
 
@@ -17,33 +18,49 @@ app.add_middleware(
 class ConnectionManager:
 	def __init__(self):
 		self.active_connections: Dict[str, WebSocket] = {}
+		self.messages: List[Dict[str, any]] = []
 
 	async def connect(self, websocket: WebSocket, client_id: str):
 		await websocket.accept()
 		self.active_connections[client_id] = websocket
 		await self.broadcast_active_connections()
-		await self.broadcast({'id': 'system', 'data': f'{client_id}님이 참여했습니다.'})
+		await self.broadcast({'id': 'system', 'data': f'{client_id}님이 참여했습니다.', 'type': 'system'})
 
 	async def disconnect(self, client_id: str):
 		self.active_connections.pop(client_id, None)
 		await self.broadcast_active_connections()
-		await self.broadcast({'id': 'system', 'data': f'{client_id}님이 나갔습니다.'})
+		await self.broadcast({'id': 'system', 'data': f'{client_id}님이 나갔습니다.', 'type': 'system'})
 
-	async def broadcast(self, message: Dict[str, str]):
+	async def broadcast(self, message: Dict[str, any]):
 		for connection in self.active_connections.values():
 			await connection.send_text(json.dumps(message))
 	
 	async def broadcast_active_connections(self):
 		user_list = list(self.active_connections.keys())
-		print(user_list)
 		active_connections_count = len(self.active_connections)
 		await self.broadcast({'type': 'active_connections', 'count': active_connections_count, 'users': user_list})
+	
+	async def handle_message(self, client_id: str, data: str):
+		message_id = len(self.messages) + 1
+		now = time
+		send_time = str(now.localtime().tm_hour) + ':' + str(now.localtime().tm_min)
+
+		message = {'id': client_id, 'data': data, 'unreadCount': len(self.active_connections) - 1, 'messageId': message_id, 'type': 'message', 'time': send_time}
+		self.messages.append(message)
+		await self.broadcast(message)
+	
+	async def mark_as_read(self, message_id: int, client_id: str):
+		for message in self.messages:
+			print(self, message)
+			if message['messageId'] == message_id and message['unreadCount'] > 0:
+				message['unreadCount'] -= 1
+				await self.broadcast({'type': 'read', 'messageId': message_id, 'unreadCount': message['unreadCount']})
 
 manager = ConnectionManager()
 
 @app.get("/")
 def hi():
-  return 'WebSocket Api Online'
+	return 'WebSocket Api Online'
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: str):
@@ -51,7 +68,11 @@ async def websocket_endpoint(websocket: WebSocket, client_id: str):
 	try:
 		while True:
 			data = await websocket.receive_text()
-			await manager.broadcast({'id': client_id, 'data': data})
+			message = json.loads(data)
+			if message['type'] == 'message':
+				await manager.handle_message(client_id, message['data'])
+			elif message['type'] == 'read':
+				await manager.mark_as_read(message['messageId'], client_id)
 	except WebSocketDisconnect:
 		await manager.disconnect(client_id)
 
--- vuechat/src/components/Message.vue
+++ vuechat/src/components/Message.vue
@@ -1,47 +1,90 @@
 <script setup>
-	import { ref, onMounted, watch, nextTick } from 'vue';
+	import '../assets/style.css'
+	import { ref, onMounted, watch, nextTick, onBeforeUnmount } from 'vue';
 	import 'emoji-picker-element';
 
-
-	const id = ref()
+	const id = ref('');
 	const ws = ref(null);
 	const input = ref('');
 	const messages = ref([]);
-	const isOpen = ref('');
+	const isOpen = ref(false);
 	const activeConnections = ref(0);
-	const activeUsers = ref();
-	const isList = ref(false)
-	const showEmojiPicker = ref(false)
+	const activeUsers = ref([]);
+	const isList = ref(false);
+	const showEmojiPicker = ref(false);
 
 	const messageContainer = ref(null);
+	let observer = null;
 
-	const scrollToBottom = () =>  {
+	const scrollToBottom = () => {
 		nextTick(() => {
 			const container = messageContainer.value;
 			if (container) {
 				container.scrollTop = container.scrollHeight;
 			}
-  });
+		});
 	};
 
 	const addEmoji = (emoji) => {
 		input.value += emoji.detail.unicode;
 		showEmojiPicker.value = false;
-	}
+	};
+
+	const markMessageAsRead = (message) => {
+		console.log(`markMessageAsRead ${document.visibilityState}`)
+		if (document.visibilityState === 'visible' && message.unreadCount > 0 && message.id != id.value && !message.isRead) {
+			ws.value.send(JSON.stringify({ type: 'read', messageId: message.messageId }));
+			message.isRead = true
+		}
+	};
+
+	const observeMessages = () => {
+		if (observer) {
+			observer.disconnect();
+		}
+
+		observer = new IntersectionObserver((entries) => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					const messageId = entry.target.dataset.messageId;
+					const message = messages.value.find(msg => msg.messageId == messageId);
+					if (message && message.unreadCount > 0) {
+						markMessageAsRead(message);
+					}
+				}
+			});
+		});
+
+		messages.value.forEach(message => {
+			const element = document.querySelector(`[data-message-id="${message.messageId}"]`);
+			if (element) {
+				observer.observe(element);
+			}
+		});
+	};
+
+	const handleVisibilityChange = () => {
+		console.log(`handleVisibilityChange ${document.visibilityState}`)
+		if (document.visibilityState === 'visible') {
+			observeMessages();
+		} else if (observer) {
+			observer.disconnect();
+		}
+	};
 
 	onMounted(() => {
-		if (localStorage.getItem('userName') == '' || localStorage.getItem('userName') == null || localStorage.getItem('userName') == 'null') {
+		if (!localStorage.getItem('userName')) {
 			const userName = prompt('사용할 이름을 입력해주세요.');
-			localStorage.setItem('userName', userName);
+			if (userName) {
+				localStorage.setItem('userName', userName);
+			} else {
+				alert('사용자 이름을 반드시 입력해주세요.');
+				window.location.reload();
+			}
 		}
 
-		if (localStorage.getItem('userName') == '' || localStorage.getItem('userName') == null || localStorage.getItem('userName') == 'null') {
-			alert('사용자 이름을 반드시 입력해주세요.')
-			window.location.reload()
-		}
-
-		id.value = localStorage.getItem('userName') || null;
-		ws.value = new WebSocket(`ws://localhost:18852/ws/${id.value}`);
+		id.value = localStorage.getItem('userName');
+		ws.value = new WebSocket(`ws://222.114.53.153:18852/ws/${id.value}`);
 
 		ws.value.onopen = () => {
 			console.log('웹소켓 연결');
@@ -51,16 +94,21 @@
 		ws.value.onmessage = (e) => {
 			const message = JSON.parse(e.data);
 			if (message.type === 'active_connections') {
-				activeUsers.value = message.users
+				activeUsers.value = message.users;
 				activeConnections.value = message.count;
-			} else {
-				messages.value.push({ id: message.id, text: message.data });
+			} else if (message.type === 'message' || message.type === 'system') {
+				messages.value.push({ ...message, read: false, isRead: false });
+			} else if (message.type === 'read') {
+				const msg = messages.value.find(m => m.messageId === message.messageId);
+				if (msg) {
+					msg.unreadCount = message.unreadCount;
+				}
 			}
 		};
 
 		ws.value.onerror = (error) => {
 			console.error('웹소켓 에러:', error);
-			isOpen.value = false
+			isOpen.value = false;
 		};
 
 		ws.value.onclose = (event) => {
@@ -69,70 +117,88 @@
 
 		watch(messages.value, () => {
 			scrollToBottom();
+			if (document.visibilityState === 'visible') {
+				nextTick(observeMessages);
+			}
 		});
+
+		document.addEventListener('visibilitychange', handleVisibilityChange);
+	});
+
+	onBeforeUnmount(() => {
+		document.removeEventListener('visibilitychange', handleVisibilityChange);
+		if (observer) {
+			observer.disconnect();
+		}
 	});
 
 	const sendMessage = () => {
 		if(input.value !== '') {
-			ws.value.send(input.value);
-			input.value = ''
+			const message = { type: 'message', data: input.value };
+			ws.value.send(JSON.stringify(message));
+			input.value = '';
 		}
 	};
 
 </script>
 
 <template>
-	<div style="height: 90%;">
-		<div>
-			채팅방 
-			<span v-if="activeConnections > 0" style="margin-left: 5px;">
-				({{ activeConnections }}명 접속 중)
-			</span>
-		</div>
-		<div class="user-list-btn">
-			<label class="font-semibold text-xl text-gray" @click="isList = !isList">
-				접속자 확인 ▼
-			</label>
-			<div v-show="isList" class="dropdown-menu">
-				<div v-for="(user, index) in activeUsers" :key="index" class="user-div">
-					{{ index + 1 }}. {{ user }}
-				</div>
-			</div>
-		</div>
-		<div style="margin-top: 10px;">
-			<span v-if="isOpen == true">
-				연결되었습니다.
-			</span>
-			<span v-else-if="isOpen == false">
-				연결에 실패했습니다.
-			</span>
-			<span v-else>
-				연결중...
-			</span>
-		</div>
+  <div style="height: 90%;">
+    <div>
+      채팅방 
+      <span v-if="activeConnections > 0" style="margin-left: 5px;">
+        ({{ activeConnections }}명 접속 중)
+      </span>
+    </div>
+    <div class="user-list-btn" style="text-align: center;">
+      <label class="font-semibold text-xl text-gray" @click="isList = !isList">
+        접속자 확인 ▼
+      </label>
+      <div v-show="isList" class="dropdown-menu">
+        <div v-for="(user, index) in activeUsers" :key="index" class="user-div">
+          {{ index + 1 }}. {{ user }}
+        </div>
+      </div>
+    </div>
+    <div style="margin-top: 10px;">
+      <span v-if="isOpen">
+        연결되었습니다.
+      </span>
+      <span v-else-if="isOpen === false">
+        연결에 실패했습니다.
+      </span>
+      <span v-else>
+        연결중...
+      </span>
+    </div>
     <div class="message-container" ref="messageContainer">
-			<div v-for="message in messages" :key="message.text" :class="{ 'text-right': message.id === id, 'text-left': message.id !== id && message.id != 'system', 'text-center': message.id == 'system' }">
-				<strong>{{ message.id }}</strong> 
-				<div :class="{ 'paddingTop': message.id != 'system' }">
+      <div v-for="message in messages" :key="message.messageId" :class="{ 'text-right': message.id === id, 'text-left': message.id !== id && message.id != 'system', 'text-center': message.id == 'system' }" :data-message-id="message.messageId">
+        <strong>{{ message.id }}</strong> 
+        <div :class="{ 'paddingTop': message.id != 'system' }" 
+					:style="{ display: message.id === 'system' ? 'block' : 'flex', 
+										flexDirection: message.id !== 'system' && message.id !== id ? 'row' : message.id === id ? 'row-reverse' : 'none', 
+										alignItems: 'center' }"
+				>
 					<span :class="{ 'own-message': message.id === id, 'other-message': message.id !== id && message.id != 'system', 'center-message': message.id == 'system' }">
-						{{ message.text }} 
-					</span>
-				</div>
-			</div>
-		</div>
+						{{ message.data }} 
+          </span>
+					<span v-if="message.id != 'system' && message.unreadCount != 0" class="unread-count">{{ message.unreadCount }}</span>
+        </div>
+				<span>
+					{{ message.time }}
+				</span>
+      </div>
+    </div>
   </div>
-	<div style="display: flex; justify-content: center; align-items: center;">
-		<input v-model="input" @keyup.enter="sendMessage" placeholder="메세지를 입력하세요." />
-		<button @click="showEmojiPicker = !showEmojiPicker">😀</button>
-		<div v-if="showEmojiPicker" class="emoji-picker-popup">
-			<emoji-picker @emoji-click="addEmoji"></emoji-picker>
-		</div>
-		<button class="submit-btn" @click="sendMessage">
-			전송
-		</button>
-	</div>
+  <div style="display: flex; justify-content: center; align-items: center; margin-top: 10px; position: relative;">
+    <input v-model="input" @keyup.enter="sendMessage" placeholder="메세지를 입력하세요." />
+    <button @click="showEmojiPicker = !showEmojiPicker">😀</button>
+    <div v-if="showEmojiPicker" class="emoji-picker-popup">
+      <emoji-picker @emoji-click="addEmoji"></emoji-picker>
+    </div>
+    <button class="submit-btn" @click="sendMessage">
+      전송
+    </button>
+  </div>
 </template>
 
-<style scoped>
-	@import '../assets/style.css'
-</style>
